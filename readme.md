8d3f8e05b8d8fdfc855f496e4a59eb7725d058c2
Norbert Schneider <norbert.schneider@codecentric.de>
2019-10-08 20:54:53 +0200

    Init. Add initial Argos USBGuard script

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Argos-USBGuard
+
+This [Argos](https://github.com/p-e-w/argos) script shows by [USBGuard](https://usbguard.github.io/) blocked devices and allows the easy acceptance of them.
+
+![Screenshot](https://github.com/bertschneider/argos-usbguard/screenshot.png)
+
+## Features
+
+- Easy acceptance of blocked USB devices
+- Only show a tray icon if blocked USB devices are present
+
+## Usage
+
+1. Clone the repository
+
+        > git clone git@github.com:bertschneider/argos-usbguard.git
+
+2. Link the script to your Argos script directory
+
+        argos-usbguard> ln ./usbguard.10s.sh ~/.config/argos
+
+3. *USBGuard needs root access* and to elevate the current user rights `sudo` is
+   used. The password will be read from the system key store using
+   `secret-tools`. So you have to access your password to the key store with the
+   following command:
+
+        > secret-tool store --label='Password for Argos-USBGuard Script' argos-usbguard password
---
+++ usbguard.10s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Before run add "argos-usbguard" to the system key-chain using:
+# > secret-tool store --label='Password for Argos-USBGuard Script' argos-usbguard password
+
+# Use a " " before sudo commands to prevent adding the password to the history
+
+REGEX='(.*): (allow)?(block)? id (.*) serial "(.*)" name "(.*)" hash "(.*)" parent-hash "(.*)" with-interface (.*)'
+PASSWORD=$(secret-tool lookup argos-usbguard password)
+BLOCK_COUNT=$( sudo -Sk -p '' <<< $PASSWORD usbguard list-devices -b | wc -l)
+
+if [ $BLOCK_COUNT != '0' ]; then
+    echo "${BLOCK_COUNT} | iconName='usb' | color='red'"
+    echo "---"
+    BLOCKED=$( sudo -Sk -p '' <<< $PASSWORD usbguard list-devices -b)
+    while read -r DEVICE; do
+        if [[ $DEVICE =~ $REGEX ]]; then
+            echo "\033[31;1;1m :no_entry: \033[0m ${BASH_REMATCH[6]}"
+            echo "--ID: ${BASH_REMATCH[4]}"
+            echo "--Serial: ${BASH_REMATCH[5]}"
+            echo "--Name: ${BASH_REMATCH[6]}"
+            echo "--Hash: ${BASH_REMATCH[7]}"
+
+            ALLOW_COMMAND=" sudo -Sk -p '' <<< ${PASSWORD} usbguard allow-device ${BASH_REMATCH[1]}"
+            echo "--:+1: ALLOW :+1:| emojize=true color='green' refresh=true terminal=false bash=\"${ALLOW_COMMAND}\""
+        fi
+    done <<< $BLOCKED
+else
+    echo "--"
+fi;
